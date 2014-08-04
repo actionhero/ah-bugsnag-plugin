dbb153dba0f8c5c361e6789f0c3769e1efa6bb7e
Evan Tahler <evan@taskrabbit.com>
2014-08-04 15:24:04 -0700

    sanitize connection

--- initializers/bugsnag.js
+++ initializers/bugsnag.js
@@ -7,6 +7,15 @@ exports.bugsnag = function(api, next){
   api.bugsnag.client.register(api.config.bugsnag.apiKey, api.config.bugsnag.options);
 
   api.bugsnag.notifier = function(err, type, name, objects, severity){
+    var relevantObjects = objects;
+    if(type == 'action'){
+      var relevantDetails = ['action', 'remoteIP', 'type', 'params', 'room'];
+      relevantObjects.connection = {};
+      relevantDetails.forEach(function(key){
+        relevantObjects.connection[key] = objects.connection[key];
+      });
+    }
+
     api.bugsnag.client.notify(err, {
       errorName: name,
       groupingHash: name,
