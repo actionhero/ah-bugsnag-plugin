f9b3a0b5155b3f7a9fb266b6b42f29a408759d71
Evan Tahler <evan@taskrabbit.com>
2015-01-12 14:01:39 -0800

    update for ah v10

--- initializers/bugsnag.js
+++ initializers/bugsnag.js
@@ -1,41 +1,40 @@
-exports.bugsnag = function(api, next){  
+module.exports = {
+  initialize: function(api, next){
+    api.bugsnag = {
+      client: require("bugsnag"),
+    };
 
-  api.bugsnag = {
-    client: require("bugsnag"),
-  }
+    api.bugsnag.client.register(api.config.bugsnag.apiKey, api.config.bugsnag.options);
 
-  api.bugsnag.client.register(api.config.bugsnag.apiKey, api.config.bugsnag.options);
+    api.bugsnag.notifier = function(err, type, name, objects, severity){
+      var relevantObjects = {};
+      if(type == 'action'){
+        var relevantDetails = ['id', 'action', 'remoteIP', 'type', 'params', 'room'];
+        relevantObjects.connection = {};
+        relevantDetails.forEach(function(key){
+          relevantObjects.connection[key] = objects.connection[key];
+        });
+      }else{
+        relevantObjects = objects;
+      }
 
-  api.bugsnag.notifier = function(err, type, name, objects, severity){
-    var relevantObjects = {};
-    if(type == 'action'){
-      var relevantDetails = ['id', 'action', 'remoteIP', 'type', 'params', 'room'];
-      relevantObjects.connection = {};
-      relevantDetails.forEach(function(key){
-        relevantObjects.connection[key] = objects.connection[key];
+      api.bugsnag.client.notify(err, {
+        errorName: name,
+        groupingHash: name,
+        type: type, 
+        objects: relevantObjects
       });
-    }else{
-      relevantObjects = objects;
-    }
+    };
+  },
 
-    api.bugsnag.client.notify(err, {
-      errorName: name,
-      groupingHash: name,
-      type: type, 
-      objects: relevantObjects
-    });
-  }
-
-  api.bugsnag._start = function(api, next){
+  start: function(api, next){
     if(api.env != 'test'){
       api.exceptionHandlers.reporters.push(api.bugsnag.notifier);
     }
     next();
-  };
+  },
 
-  api.bugsnag._stop =  function(api, next){
+  stop: function(api, next){
     next();
-  };
-
-  next();
-}
+  },
+};
